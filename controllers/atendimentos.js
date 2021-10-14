00a8f7858fba5957e185000f176fae9522c3f23d
Paulo Martins <p.o.marttins@gmail.com>
2021-10-14 01:06:49 -0300

    Pratica de aula NodeJS

---
+++ controllers/atendimentos.js
@@ -0,0 +1,32 @@
+const Atendimento = require('../models/atendimentos')
+
+module.exports = app => {
+  app.get('/atendimentos', (req, res) => {
+    Atendimento.lista(res)
+  })
+
+  app.get('/atendimento/:id', (req, res) => {
+    const id = parseInt(req.params.id)
+
+    Atendimento.buscaPorId(id, res)
+  })
+
+  app.post('/atendimentos', (req, res) => {
+    const atendimento = req.body
+
+    Atendimento.adiciona(atendimento, res)
+  })
+
+  app.patch('/atendimento/:id', (req, res) => {
+    const id = parseInt(req.params.id)
+    const valores = req.body
+
+    Atendimento.altera(id, valores, res)
+  })
+
+  app.delete('/atendimento/:id', (req, res) => {
+    const id = parseInt(req.params.id)
+
+    Atendimento.delete(id, res)
+  })
+}
